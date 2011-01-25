a53daaa0176e315f550b954772ae84b02a857aed
Postmodern <postmodern.mod3@gmail.com>
2011-01-25 14:54:18 -0800

    Copied specs for rack-jetty.

---
+++ spec/rack/handler/helpers/test_request.rb
@@ -0,0 +1,69 @@
+require 'net/http'
+require 'yaml'
+
+# Borrowed from Rack's own specs.
+class TestRequest
+  def call(env)
+    status = env["QUERY_STRING"] =~ /secret/ ? 403 : 200
+    env["test.postdata"] = env["rack.input"].read
+    body = env.to_yaml
+    size = body.respond_to?(:bytesize) ? body.bytesize : body.size
+    [status, {"Content-Type" => "text/yaml", "Content-Length" => size.to_s}, [body]]
+  end
+
+  module Helpers
+    attr_reader :status, :response
+
+    ROOT = File.expand_path(File.dirname(__FILE__) + "/..")
+    ENV["RUBYOPT"] = "-I#{ROOT}/lib -rubygems"
+
+    def root
+      ROOT
+    end
+
+    def rackup
+      "#{ROOT}/bin/rackup"
+    end
+
+    def GET(path, header={})
+      Net::HTTP.start(@host, @port) { |http|
+        user = header.delete(:user)
+        passwd = header.delete(:passwd)
+
+        get = Net::HTTP::Get.new(path, header)
+        get.basic_auth user, passwd if user && passwd
+        http.request(get) { |response|
+          @status = response.code.to_i
+          if response.content_type == "text/yaml"
+            load_yaml(response)
+          else
+            @response = response
+          end
+        }
+      }
+    end
+
+    def POST(path, formdata={}, header={})
+      Net::HTTP.start(@host, @port) { |http|
+        user = header.delete(:user)
+        passwd = header.delete(:passwd)
+
+        post = Net::HTTP::Post.new(path, header)
+        post.form_data = formdata
+        post.basic_auth user, passwd if user && passwd
+        http.request(post) { |response|
+          @status = response.code.to_i
+          @response = YAML.load(response.body)
+        }
+      }
+    end
+    
+    def load_yaml(response)
+      begin
+        @response = YAML.load(response.body)
+      rescue ArgumentError
+        @response = nil
+      end
+    end
+  end
+end
---
+++ spec/rack/handler/http_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+require 'rack/handler/helpers/test_request'
+
+require 'rack/handler/http'
+require 'rack/lint'
+require 'rack/builder'
+require 'rack/static'
+
+describe Rack::Handler::HTTP do
+  include TestRequest::Helpers
+
+  let(:host) { '0.0.0.0' }
+  let(:port) { 9204 }
+  
+  before(:all) do
+    app = Rack::Builder.app do
+      use Rack::Lint
+      use Rack::Static, :urls => ["/spec/images"]
+      run TestRequest.new
+    end
+    
+    @server = Rack::Handler::HTTP.new(app, :Host => host, :Port => port)
+
+    Thread.new { @server.run }
+    Thread.pass until @server.running?
+  end
+  
+  after(:all) do
+    @server.stop
+    Thread.pass until @server.stopped?
+  end
+  
+  it "should respond to a simple get request" do
+    GET "/"
+    status.should == 200
+  end
+  
+  it "should have CGI headers on GET" do
+    GET("/")
+    response["REQUEST_METHOD"].should == "GET"
+    response["SCRIPT_NAME"].should == ''
+    response["PATH_INFO"].should == "/"
+    response["QUERY_STRING"].should == ""
+    response["test.postdata"].should == ""
+
+    GET("/test/foo?quux=1")
+    response["REQUEST_METHOD"].should == "GET"
+    response["SCRIPT_NAME"].should == ''
+    response["REQUEST_URI"].should == "/test/foo"
+    response["PATH_INFO"].should == "/test/foo"
+    response["QUERY_STRING"].should == "quux=1"
+  end
+
+  it "should have CGI headers on POST" do
+    POST("/", {"rack-form-data" => "23"}, {'X-test-header' => '42'})
+    status.should == 200
+    response["REQUEST_METHOD"].should == "POST"
+    response["REQUEST_URI"].should == "/"
+    response["QUERY_STRING"].should == ""
+    response["HTTP_X_TEST_HEADER"].should == "42"
+    response["test.postdata"].should == "rack-form-data=23"
+  end
+
+  it "should support HTTP auth" do
+    GET("/test", {:user => "ruth", :passwd => "secret"})
+    response["HTTP_AUTHORIZATION"].should == "Basic cnV0aDpzZWNyZXQ="
+  end
+
+  it "should set status" do
+    GET("/test?secret")
+    status.should == 403
+    response["rack.url_scheme"].should == "http"
+  end
+
+  it "should not set content-type to '' in requests" do
+    GET("/test", 'Content-Type' => '')
+    response['Content-Type'].should == nil
+  end
+  
+  it "should serve images" do
+    file_size = File.size(File.join(File.dirname(__FILE__), 'images', 'image.jpg'))
+    GET("/spec/images/image.jpg")
+    status.should == 200
+    response.content_length.should == file_size
+    response.body.size.should == file_size
+  end
+end
