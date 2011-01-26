b705ac1b9b433fef13753fab5747fff20d7282d3
Postmodern <postmodern.mod3@gmail.com>
2011-01-26 00:41:22 -0800

    Added specs for read_request.

--- spec/server/requests_spec.rb
+++ spec/server/requests_spec.rb
@@ -1,8 +1,41 @@
 require 'spec_helper'
 require 'net/http/server/requests'
 
+require 'stringio'
+
 describe Net::HTTP::Server::Requests do
   include Net::HTTP::Server::Requests
+
+  describe "read_request" do
+    it "should ignore requests that do not contain an HTTP version" do
+      stream = StringIO.new("GET /\r\n")
+
+      read_request(stream).should be_nil
+    end
+
+    it "should ignore requests with malformed headers" do
+      stream = StringIO.new("GET / HTTP/1.1\r\nFoo: Bar\r\nBAZ\r\n\r\n")
+
+      read_request(stream).should be_nil
+    end
+
+    it "should read requests with an HTTP Version and Headers" do
+      request = "GET / HTTP/1.1\r\nFoo: Bar\r\n\r\n"
+      stream = StringIO.new(request)
+
+      read_request(stream).should == request
+    end
+
+    it "should not read the body of the request" do
+      request = "GET / HTTP/1.1\r\nFoo: Bar\r\n\r\n"
+      body = "<html>\r\n\t<body>hello</body>\r\n</html>\r\n"
+      stream = StringIO.new(request + body)
+
+      read_request(stream)
+
+      stream.read.should == body
+    end
+  end
 
   describe "normalize_uri" do
     it "should infer the :port, if :scheme is also set" do
