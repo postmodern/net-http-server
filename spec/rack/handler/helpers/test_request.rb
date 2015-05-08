8a8051c5e68d72a8e1c8d3d8d3ef98c4c687b0a8
Postmodern <postmodern.mod3@gmail.com>
2015-05-08 15:04:26 -0700

    Updated TestRequest against rack-jetty.
    * https://github.com/stormbrew/rack-jetty/blob/38574e179d10ae64432565d725d4ce0684c484e5/spec/spec_helper.rb

--- spec/rack/handler/helpers/test_request.rb
+++ spec/rack/handler/helpers/test_request.rb
@@ -6,6 +6,15 @@ class TestRequest
   def call(env)
     status = env["QUERY_STRING"] =~ /secret/ ? 403 : 200
     env["test.postdata"] = env["rack.input"].read
+    # clean out complex objects that won't translate over anyways.
+    env.keys.each do |key|
+      case env[key]
+      when String, Fixnum, Bignum
+        # leave it
+      else
+        env[key] = nil
+      end
+    end
     body = env.to_yaml
     size = body.respond_to?(:bytesize) ? body.bytesize : body.size
     [status, {"Content-Type" => "text/yaml", "Content-Length" => size.to_s}, [body]]
@@ -31,7 +40,7 @@ class TestRequest
         passwd = header.delete(:passwd)
 
         get = Net::HTTP::Get.new(path, header)
-        get.basic_auth user, passwd if user && passwd
+        get.basic_auth user, passwd  if user && passwd
         http.request(get) { |response|
           @status = response.code.to_i
           if response.content_type == "text/yaml"
@@ -50,14 +59,14 @@ class TestRequest
 
         post = Net::HTTP::Post.new(path, header)
         post.form_data = formdata
-        post.basic_auth user, passwd if user && passwd
+        post.basic_auth user, passwd  if user && passwd
         http.request(post) { |response|
           @status = response.code.to_i
           @response = YAML.load(response.body)
         }
       }
     end
-    
+
     def load_yaml(response)
       begin
         @response = YAML.load(response.body)
